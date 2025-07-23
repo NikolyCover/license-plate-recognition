b955616d1df637c57160cc58c885e78a53f7cba2
Nikoly Cover <nikoly.pereira@itaipuparquetec.org.br>
2025-07-22 21:31:41 -0300

    fix: erosion using morph cross structuring element

--- src/erosion.py
+++ src/erosion.py
@@ -1,9 +1,14 @@
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 
-def apply_erosion(binary_image, iterations=1):
-    kernel = np.ones((3, 3), np.uint8)
+def apply_erosion(binary_image, iterations=1, invert=True):
+    if invert:
+        binary_image = cv2.bitwise_not(binary_image)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+
     eroded = cv2.erode(binary_image, kernel, iterations=iterations)
+
+    if invert:
+        eroded = cv2.bitwise_not(eroded)
 
     return eroded
